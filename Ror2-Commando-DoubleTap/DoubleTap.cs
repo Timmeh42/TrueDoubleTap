1c8e9102cbf28e5c5ee61944639f79bd3bcd6728
Tim <tmeier42+github@gmail.com>
2019-04-18 21:20:27 +0200

    first draft

---
+++ Ror2-Commando-DoubleTap/DoubleTap.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using BepInEx;
+using AetherLib;
+
+namespace TrueDoubleTap
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.Timmeh42.TrueDoubleTap", "True DoubleTap", "1.0.0")]
+    public class TrueDoubleTap : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            On.EntityStates.Commando.CommandoWeapon.FirePistol2.OnEnter += (orig, self) =>
+            {
+
+                Assembly assembly = self.GetType().Assembly;
+                Type firePistol = assembly.GetClass("EntityStates.Commando.CommandoWeapon", "FirePistol2");
+
+                float curdur = (float) firePistol.GetPublicStaticFieldInfo("baseDuration").GetValue(null);
+
+                FieldInfo remainingShots = firePistol.GetField("remainingShots", BindingFlags.Public | BindingFlags.Instance);
+
+                firePistol.SetPublicStaticField("baseDuration", 0.5f * curdur);
+                if ((int) remainingShots.GetValue(self) % 2 != 0)
+                {
+                    firePistol.SetPublicStaticField("baseDuration", 1.5f * curdur);
+                }
+                orig(self);
+                firePistol.SetPublicStaticField("baseDuration", curdur);
+            };
+        }
+    }
+}
