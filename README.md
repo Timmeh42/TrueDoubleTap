1b4201cdc4510c149f0584088bcbd5cef56ee42d
Tim <tmeier42+github@gmail.com>
2019-04-19 21:08:41 +0200

    v1.3.0
     - Merged spread scaling mechanic into repo
     - Overhauled functions
     - Clarified config options
     - Shiny new icon

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # TrueDoubleTap
 
-Risk of Rain 2 mod to make the Commando really double tap.
+Risk of Rain 2 mod to improve the Commando's primary fire
 
-Instead of the commando firing at a consistent rate, he now fires a burst of two shots with a longer pause between bursts.
-The burst speed ratio (default 0.3) can be configured between 0.01 (almost instant) and 1 (same as unmodded) in the `/BepInEx/config/com.Timmeh42.TrueDoubleTap.cfg` file.
-Values are clamped to that range, or set to 1 if invalid.
-Overall his attack speed is unchanged, so this is an 'ethical' mod to use.
-Very simple mod that should be compatible with anything.
+ - Instead of the commando firing at a consistent rate, he now fires a burst of two shots with a longer pause between bursts, with the same average attack speed
+ - Now has an option to scale down the bullet spread when firing a lot (0 is a good number)
+
+
+Configuration is done in the `/BepInEx/config/com.Timmeh42.TrueDoubleTap.cfg` file, which appears after loading the game once.
+
+ - The burst speed ratio (default 0.3) can be configured between 0.01 (almost instant) and 1 (same as unmodded). Overall his attack speed is unchanged, so this is an 'ethical' mod to use.
+
+ - The bullet spread scaling (default 1, same as unmodded) can be anything 0 or higher (maybe you want MUL-T's spread?) 
 
 ### Installation
 
--- Ror2-Commando-DoubleTap/DoubleTap.cs
+++ Ror2-Commando-DoubleTap/DoubleTap.cs
@@ -8,17 +8,14 @@ using RoR2;
 namespace TrueDoubleTap
 {
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("com.Timmeh42.TrueDoubleTap", "True DoubleTap", "1.2.2")]
+    [BepInPlugin("com.Timmeh42.TrueDoubleTap", "True DoubleTap", "1.3.0")]
     public class TrueDoubleTap : BaseUnityPlugin
     {
         public float ParseFloat(string strInput, float defaultVal = 1.0f, float min = float.MinValue, float max = float.MaxValue)
         {
             if (float.TryParse(strInput, NumberStyles.Any, CultureInfo.InvariantCulture, out float parsedFloat))
             {
-                if (parsedFloat <= min) parsedFloat = min;
-                if (parsedFloat >= max) parsedFloat = max;
-
-                return parsedFloat;
+                return parsedFloat <= min ? min : parsedFloat >= max ? max : parsedFloat;
             }
             return defaultVal;
         }
@@ -28,13 +25,44 @@ namespace TrueDoubleTap
             return bool.TryParse(strInput, out bool parsedBool) ? parsedBool : defaultVal;
         }
 
-
-        float ratio => ParseFloat(Config.Wrap("DoubleTap", "Ratio", "Ratio of time between shots to time between bursts (decimal number between 0.01 and 1)", "0.3").Value, 1f, 0.01f, 1f);
-        bool chatOutput => ParseBool(Config.Wrap("DoubleTap", "ChatOutput", "Whether to write mod output to chat (boolean 'true' or 'false')", "true").Value);
+        float ratio => ParseFloat(Config.Wrap("DoubleTap", "Ratio", "Ratio of time between shots to time between bursts (decimal number between 0.01 and 1 - default 0.3)", "0.3").Value, 1f, 0.01f, 1f);
+        float spread => ParseFloat(Config.Wrap("DoubleTap", "SpreadScale", "Factor to scale the default spread by (decimal number 0 or higher - default 1 (same as unmodded))", "1.0").Value, 1f, 0f);
+        bool chatOutput => ParseBool(Config.Wrap("DoubleTap", "ChatOutput", "Whether to write mod output to chat (boolean 'true' or 'false' - default 'true')", "true").Value);
 
         public void Awake()
         {
-            if (chatOutput) Chat.AddMessage(String.Format("Commando DoubleTap ratio set to {0}", ratio));
+            if (chatOutput)
+            {
+                Chat.AddMessage(String.Format("TrueDoubleTap: burst set to {0}", ratio));
+                switch (spread)
+                {
+                    case 1f:
+                        Chat.AddMessage(String.Format("TrueDoubleTap: spread set to {0}x (default)", spread));
+                        break;
+                    case 0f:
+                        Chat.AddMessage("I guess they never miss, huh?");
+                        break;
+                    default:
+                        Chat.AddMessage(String.Format("TrueDoubleTap: spread set to {0}x", spread));
+                        break;
+                }
+            }
+
+            On.RoR2.Run.Awake += (orig, self) =>
+            {
+                orig(self);
+                if (spread != 1.0f)
+                {
+                    Assembly assembly = self.GetType().Assembly;
+                    Type firePistol = assembly.GetTypes().First(t => t.IsClass && t.Namespace == "EntityStates.Commando.CommandoWeapon" && t.Name == "FirePistol2");
+
+                    var recoilAmplitudeOld = (float) firePistol.GetField("recoilAmplitude", BindingFlags.Static | BindingFlags.Public).GetValue(self);
+                    var spreadBloomValueOld = (float)firePistol.GetField("spreadBloomValue", BindingFlags.Static | BindingFlags.Public).GetValue(self);
+
+                    firePistol.GetField("recoilAmplitude", BindingFlags.Static | BindingFlags.Public).SetValue(null, spread * recoilAmplitudeOld);
+                    firePistol.GetField("spreadBloomValue", BindingFlags.Static | BindingFlags.Public).SetValue(null, spread * spreadBloomValueOld);
+                }
+            };
 
             On.EntityStates.Commando.CommandoWeapon.FirePistol2.OnEnter += (orig, self) =>
             {
