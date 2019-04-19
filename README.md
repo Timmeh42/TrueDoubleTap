a77b84d9b579a94240f71e227931b3505df72283
Tim <tmeier42+github@gmail.com>
2019-04-19 10:37:27 +0200

    Added configurability for the burst ratio

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Risk of Rain 2 mod to make the Commando really double tap.
 
 Instead of the commando firing at a consistent rate, he now fires a burst of two shots with a longer pause between bursts.
+The burst speed ratio (default 0.3) can be configured between 0.01 (almost instant) and 1 (same as unmodded) in the `/BepInEx/config/com.Timmeh42.TrueDoubleTap.cfg` file.
+Values are clamped to that range, or set to 1 if invalid.
 Overall his attack speed is unchanged, so this is an 'ethical' mod to use.
 Very simple mod that should be compatible with anything.
 
--- Ror2-Commando-DoubleTap/DoubleTap.cs
+++ Ror2-Commando-DoubleTap/DoubleTap.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using BepInEx;
+using BepInEx.Configuration;
+using RoR2;
 
 namespace TrueDoubleTap
 {
@@ -9,18 +12,32 @@ namespace TrueDoubleTap
     [BepInPlugin("com.Timmeh42.TrueDoubleTap", "True DoubleTap", "1.0.0")]
     public class TrueDoubleTap : BaseUnityPlugin
     {
+        public float RatioConfig(string configline)
+        {
+            if (float.TryParse(configline, NumberStyles.Any, CultureInfo.InvariantCulture, out float ratioTemp))
+            {
+                var ratioMin = 0.01f;
+                var ratioMax = 1f;
+                return (ratioTemp <= ratioMin) ? ratioMin : (ratioTemp >= ratioMax) ? ratioMax : ratioTemp;
+            }
+            return 1f;
+        }
+        float Ratio => RatioConfig(Config.Wrap("DoubleTap", "Ratio", "Ratio of time between shots to time between bursts", "0.3").Value);
+
         public void Awake()
         {
+            Chat.AddMessage(String.Format("Commando DoubleTap ratio set to {0}", Ratio));
+
             On.EntityStates.Commando.CommandoWeapon.FirePistol2.OnEnter += (orig, self) =>
             {
                 Assembly assembly = self.GetType().Assembly;
                 Type firePistol = assembly.GetTypes().First(t => t.IsClass && t.Namespace == "EntityStates.Commando.CommandoWeapon" && t.Name == "FirePistol2");
                 float current_duration = (float) firePistol.GetField("baseDuration", BindingFlags.Static | BindingFlags.Public).GetValue(null);
-                firePistol.GetField("baseDuration", BindingFlags.Static | BindingFlags.Public).SetValue(null, 0.5f * current_duration);
+                firePistol.GetField("baseDuration", BindingFlags.Static | BindingFlags.Public).SetValue(null, 2f * Ratio / (1f + Ratio) * current_duration);
                 FieldInfo remainingShots = firePistol.GetField("remainingShots", BindingFlags.Public | BindingFlags.Instance);
                 if ((int) remainingShots.GetValue(self) % 2 != 0)
                 {
-                    firePistol.GetField("baseDuration", BindingFlags.Static | BindingFlags.Public).SetValue(null, 1.5f * current_duration);
+                    firePistol.GetField("baseDuration", BindingFlags.Static | BindingFlags.Public).SetValue(null, 2f * 1f / (1f + Ratio) * current_duration);
                 }
                 orig(self);
                 firePistol.GetField("baseDuration", BindingFlags.Static | BindingFlags.Public).SetValue(null, current_duration);
